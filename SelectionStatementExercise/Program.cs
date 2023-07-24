cd2e0be39f7c70ebec0ffdd6641404b0d381652f
Mack McCall <mackmc9924@gmail.com>
2023-07-24 13:48:00 -0600

    completed exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var rdmNumber = r.Next(1, 10);
+
+            Console.WriteLine("Try to guess the random number between 1 and 10! (It will change each time)");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < rdmNumber)
+            {
+                Console.WriteLine("too low");
+            }
+            else if (userInput > rdmNumber)
+            {
+                Console.WriteLine("too high");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!!!");
+            }
         }
     }
 }
